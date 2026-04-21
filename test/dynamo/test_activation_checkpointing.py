5800dd1df2eeab5d8b78c7ebffb836468751d8ad
shangerxin <shangerxin@hotmail.com>
2026-04-21 11:26:50 +0800

    Fix ut error

--- test/dynamo/test_activation_checkpointing.py
+++ test/dynamo/test_activation_checkpointing.py
@@ -3762,7 +3762,7 @@ class ActivationCheckpointingNestedCompileTests(torch._dynamo.test_case.TestCase
             def block(self, x):
                 return compiled_f(x)
 
-        m = getattr(M(), device_type)
+        m = getattr(M(), device_type)()
         x = torch.randn(8, device=device_type, requires_grad=True)
 
         def fn(x):
