f0da2b2f98d7d551d619cd5367bd311c798a6cff
shangerxin <shangerxin@hotmail.com>
2026-04-23 11:34:01 +0800

    Fix lint issue

--- test/dynamo/test_structured_trace.py
+++ test/dynamo/test_structured_trace.py
@@ -364,7 +364,7 @@ class StructuredTraceTest(TestCase):
 {{"dynamo_cpp_guards_str": {{}}, "frame_id": 0, "frame_compile_id": 0, "attempt": 0, "has_payload": "HASH"}}
 {{"compilation_metrics": "METRICS", "frame_id": 0, "frame_compile_id": 0, "attempt": 0}}
 {{"compilation_metrics_runtime": "METRICS", "frame_id": 0, "frame_compile_id": 0}}
-""",  # noqa: B950
+""",
         )
 
         self.assertParses()
@@ -398,7 +398,7 @@ class StructuredTraceTest(TestCase):
 {{"dynamo_cpp_guards_str": {{}}, "frame_id": 0, "frame_compile_id": 0, "attempt": 0, "has_payload": "HASH"}}
 {{"compilation_metrics": "METRICS", "frame_id": 0, "frame_compile_id": 0, "attempt": 0}}
 {{"compilation_metrics_runtime": "METRICS", "frame_id": 0, "frame_compile_id": 0}}
-""",  # noqa: B950
+""",
         )
 
         self.assertParses()
