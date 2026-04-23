8698f8943bcd9820dee52e7ef5f12c93e2939ddc
shangerxin <shangerxin@hotmail.com>
2026-04-23 11:22:36 +0800

    Merge conflict

--- test/dynamo/test_deviceguard.py
+++ test/dynamo/test_deviceguard.py
@@ -4,8 +4,12 @@ from unittest.mock import Mock
 import torch
 import torch._dynamo.test_case
 import torch._dynamo.testing
-from torch._dynamo.device_interface import DeviceGuard, get_interface_for_device
-from torch.testing._internal.common_device_type import instantiate_device_type_tests
+from torch._dynamo.device_interface import CudaInterface, DeviceGuard, XpuInterface
+from torch.testing._internal.common_utils import TEST_XPU, TEST_CUDA
+
+device_type = acc.type if (acc := torch.accelerator.current_accelerator()) else "cpu"
+TEST_GPU = TEST_CUDA or TEST_XPU
+TEST_MULTIGPU = torch.cuda.device_count() >= 2 or torch.xpu.device_count() >= 2
 
 
 class TestDeviceGuard(torch._dynamo.test_case.TestCase):
@@ -45,19 +49,41 @@ class TestDeviceGuard(torch._dynamo.test_case.TestCase):
         self.assertEqual(device_guard.idx, None)
 
 
-class TestDeviceGuardWithInterface(torch._dynamo.test_case.TestCase):
+@unittest.skipIf(not TEST_GPU, "No GPU available.")
+class TestGPUDeviceGuard(torch._dynamo.test_case.TestCase):
     """
-    Unit tests for the DeviceGuard class using a real DeviceInterface.
+    Unit tests for the DeviceGuard class using a GPU=Interface.
     """
+
+        if device_type == "cuda":
+        self.device_interface = CudaInterface
+        elif device_type == "xpu":
+            self.device_interface = XpuInterface
+        else:
+            raise ValueError("Not supported GPU type")
+    @unittest.skipIf(not TEST_MULTIGPU, "need multiple GPU")
+    def test_device_guard(self):
+        current_device = torch.accelerator.current_device_index()
+
+        device_guard = DeviceGuard(self.device_interface, 1)
+
+        with device_guard as _:
+            self.assertEqual(torch.accelerator.current_device_index(), 1)
+            self.assertEqual(device_guard.prev_idx, 0)
+            self.assertEqual(device_guard.idx, 1)
+
+        self.assertEqual(torch.accelerator.current_device_index(), current_device)
+        self.assertEqual(device_guard.prev_idx, 0)
+        self.assertEqual(device_guard.idx, 1)
 
     def test_device_guard_no_index(self, device):
         device_interface = get_interface_for_device(torch.device(device).type)
-        current_device = device_interface.current_device()
+        current_device = torch.accelerator.current_device_index()
 
         device_guard = DeviceGuard(device_interface, None)
 
         with device_guard as _:
-            self.assertEqual(device_interface.current_device(), current_device)
+            self.assertEqual(torch.cuda.current_device(), current_device)
             self.assertEqual(device_guard.prev_idx, -1)
             self.assertEqual(device_guard.idx, None)
 
